0c7780da58088620aa28cdc964711a3e3eeb63be
Jethro Kuan <jethrokuan95@gmail.com>
2018-09-14 16:45:13 +0800

    yapfify logger

--- utils/logger.py
+++ utils/logger.py
@@ -26,10 +26,10 @@ class DefinedSummarizer:
 
         if "comet_api_key" in config:
             from comet_ml import Experiment
-            self.experiment = Experiment(api_key=config['comet_api_key'], project_name=config['exp_name'])
+            self.experiment = Experiment(
+                api_key=config['comet_api_key'],
+                project_name=config['exp_name'])
             self.experiment.log_multiple_params(config)
-
-
 
     def set_summaries(self, scalar_tags=None, images_tags=None):
         self.scalar_tags = scalar_tags
@@ -41,13 +41,17 @@ class DefinedSummarizer:
             if self.scalar_tags is not None:
                 for tag in self.scalar_tags:
                     self.summary_tags += [tag]
-                    self.summary_placeholders[tag] = tf.placeholder('float32', None, name=tag)
-                    self.summary_ops[tag] = tf.summary.scalar(tag, self.summary_placeholders[tag])
+                    self.summary_placeholders[tag] = tf.placeholder(
+                        'float32', None, name=tag)
+                    self.summary_ops[tag] = tf.summary.scalar(
+                        tag, self.summary_placeholders[tag])
             if self.images_tags is not None:
                 for tag, shape in self.images_tags:
                     self.summary_tags += [tag]
-                    self.summary_placeholders[tag] = tf.placeholder('float32', shape, name=tag)
-                    self.summary_ops[tag] = tf.summary.image(tag, self.summary_placeholders[tag], max_outputs=10)
+                    self.summary_placeholders[tag] = tf.placeholder(
+                        'float32', shape, name=tag)
+                    self.summary_ops[tag] = tf.summary.image(
+                        tag, self.summary_placeholders[tag], max_outputs=10)
 
     def summarize(self, step, summaries_dict=None, summaries_merged=None):
         """
@@ -58,9 +62,11 @@ class DefinedSummarizer:
         :return:
         """
         if summaries_dict is not None:
-            summary_list = self.sess.run([self.summary_ops[tag] for tag in summaries_dict.keys()],
-                                         {self.summary_placeholders[tag]: value for tag, value in
-                                          summaries_dict.items()})
+            summary_list = self.sess.run(
+                [self.summary_ops[tag] for tag in summaries_dict.keys()], {
+                    self.summary_placeholders[tag]: value
+                    for tag, value in summaries_dict.items()
+                })
             for summary in summary_list:
                 self.summary_writer.add_summary(summary, step)
         if summaries_merged is not None:
@@ -69,29 +75,35 @@ class DefinedSummarizer:
             if hasattr(self, 'experiment') and self.experiment is not None:
                 self.experiment.log_multiple_metrics(summaries_dict, step=step)
 
-
     def finalize(self):
         self.summary_writer.flush()
 
 
 class Logger:
-    def __init__(self, sess,config):
+    def __init__(self, sess, config):
         self.sess = sess
         self.config = config
         self.summary_placeholders = {}
         self.summary_ops = {}
-        self.train_summary_writer = tf.summary.FileWriter(os.path.join(self.config["summary_dir"], "train"),
-                                                          self.sess.graph)
-        self.test_summary_writer = tf.summary.FileWriter(os.path.join(self.config["summary_dir"], "test"))
+        self.train_summary_writer = tf.summary.FileWriter(
+            os.path.join(self.config["summary_dir"], "train"), self.sess.graph)
+        self.test_summary_writer = tf.summary.FileWriter(
+            os.path.join(self.config["summary_dir"], "test"))
 
         if "comet_api_key" in config:
             from comet_ml import Experiment
-            self.experiment = Experiment(api_key=config['comet_api_key'], project_name=config['exp_name'])
+            self.experiment = Experiment(
+                api_key=config['comet_api_key'],
+                project_name=config['exp_name'])
             self.experiment.disable_mp()
             self.experiment.log_multiple_params(config)
 
     # it can summarize scalars and images.
-    def summarize(self, step, summarizer="train", scope="", summaries_dict=None):
+    def summarize(self,
+                  step,
+                  summarizer="train",
+                  scope="",
+                  summaries_dict=None):
         """
         :param step: the step of the summary
         :param summarizer: use the train summary writer or the test one
@@ -107,21 +119,28 @@ class Logger:
                 for tag, value in summaries_dict.items():
                     if tag not in self.summary_ops:
                         if len(value.shape) <= 1:
-                            self.summary_placeholders[tag] = tf.placeholder('float32', value.shape, name=tag)
+                            self.summary_placeholders[tag] = tf.placeholder(
+                                'float32', value.shape, name=tag)
                         else:
-                            self.summary_placeholders[tag] = tf.placeholder('float32', [None] + list(value.shape[1:]),
-                                                                            name=tag)
+                            self.summary_placeholders[tag] = tf.placeholder(
+                                'float32', [None] + list(value.shape[1:]),
+                                name=tag)
                         if len(value.shape) <= 1:
-                            self.summary_ops[tag] = tf.summary.scalar(tag, self.summary_placeholders[tag])
+                            self.summary_ops[tag] = tf.summary.scalar(
+                                tag, self.summary_placeholders[tag])
                         else:
-                            self.summary_ops[tag] = tf.summary.image(tag, self.summary_placeholders[tag])
+                            self.summary_ops[tag] = tf.summary.image(
+                                tag, self.summary_placeholders[tag])
 
-                    summary_list.append(self.sess.run(self.summary_ops[tag], {self.summary_placeholders[tag]: value}))
+                    summary_list.append(
+                        self.sess.run(self.summary_ops[tag],
+                                      {self.summary_placeholders[tag]: value}))
 
                 for summary in summary_list:
                     summary_writer.add_summary(summary, step)
 
-                if hasattr(self,'experiment') and self.experiment is not None:
-                    self.experiment.log_multiple_metrics(summaries_dict, step=step)
+                if hasattr(self, 'experiment') and self.experiment is not None:
+                    self.experiment.log_multiple_metrics(
+                        summaries_dict, step=step)
 
                 summary_writer.flush()
