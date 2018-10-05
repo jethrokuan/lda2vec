c5ecd0ee227df15e3466075968b9fa023c716dfe
Jethro Kuan <jethrokuan95@gmail.com>
2018-10-05 13:46:27 +0800

    Clean up estimator code

--- estimators/lda2vec.py
+++ estimators/lda2vec.py
@@ -19,37 +19,30 @@ def lda2vec_model_fn(features, labels, mode, params):
         loss = (alpha - 1.0) * log_proportions
         return tf.reduce_sum(loss)
 
-    def _embed_mixture(num_docs,
-                       num_topics,
-                       embedding_size,
-                       name):
-        scalar = 1 / np.sqrt(num_docs + num_topics)
-        document_embedding = tf.Variable(
-            tf.random_normal(
-                [num_docs, num_topics], mean=0, stddev=50 * scalar),
-            name=name)
-        with tf.name_scope("{}_Topics".format(name)):
-            topic_embedding = tf.get_variable(
-                "{}_topic_embedding".format(name),
-                shape=[params["num_topics"], embedding_size],
-                dtype=tf.float32,
-                initializer=tf.orthogonal_initializer(gain=scalar))
+    with tf.variable_scope("embeddings", reuse=tf.AUTO_REUSE):
+        scalar = 1 / np.sqrt(params["num_documents"] + params["num_topics"])
+        word_embedding = tf.get_variable(
+            "word_embedding",
+            shape=[params["vocabulary_size"],
+                   params["embedding_size"]],
+            dtype=tf.float32,
+            initializer=tf.initializers.random_normal())
+        topic_embedding = tf.get_variable(
+            "topic_embedding",
+            shape=[params["num_topics"], params["embedding_size"]],
+            dtype=tf.float32,
+            initializer=tf.orthogonal_initializer(gain=scalar))
+        document_embedding = tf.get_variable(
+            "document_embedding",
+            shape=[params["num_documents"], params["num_topics"]],
+            dtype=tf.float32,
+            initializer=tf.initializers.random_normal(
+                mean=0.0,
+                stddev= 50 * scalar))
 
-        return document_embedding, topic_embedding
-
-    word_embeddings = tf.Variable(
-        tf.random_uniform(
-            [params["vocabulary_size"],
-             params["embedding_size"]], -1.0,1.0
-        ),
-        name="word_embedding"
-    )
     word_context = tf.nn.embedding_lookup(
-        word_embeddings, features["target"], name="word_context")
-    document_embedding, topic_embedding = _embed_mixture(
-        params["num_documents"], params["num_topics"],
-        params["embedding_size"], name="document"
-    )
+        word_embedding, features["target"], name="word_context")
+
     document_proportions = tf.nn.embedding_lookup(
         document_embedding,
         features["doc_id"],
