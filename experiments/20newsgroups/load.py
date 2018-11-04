83c0dd6b13475ec31d1cc533b0047011fd7f5b98
Jethro Kuan <jethrokuan95@gmail.com>
2018-11-04 22:36:01 +0800

    get no_oov data

--- experiments/20newsgroups/load.py
+++ experiments/20newsgroups/load.py
@@ -10,7 +10,7 @@ from collections import defaultdict
 
 CORPUS = "data/twenty_newsgroups/train.txt"
 texts = read_file(CORPUS)
-SAVE_DIR = "data/twenty_newsgroups/"
+SAVE_DIR = "data/twenty_newsgroups/no_oov/"
 create_dirs([SAVE_DIR])
 
 _OOV_TOKEN = "<OOV>"
@@ -45,12 +45,14 @@ with tf.python_io.TFRecordWriter(tfrecord_path) as writer:
             negative_samples=0)
         if len(pairs) > 2:
             for pair in pairs:
-                feature = {
-                    "target": _int64_feature(pair[0]),
-                    "context": _int64_feature(pair[1]),
-                    "doc_id": _int64_feature(idx)
-                }
-                example = tf.train.Example(features=tf.train.Features(feature=feature))
+                if _OOV_TOKEN_ID not in pair:
+                    feature = {
+                        "target": _int64_feature(pair[0]),
+                        "context": _int64_feature(pair[1]),
+                        "doc_id": _int64_feature(idx)
+                    }
+                example = tf.train.Example(
+                    features=tf.train.Features(feature=feature))
                 writer.write(example.SerializeToString())
 
 
