77ab237326f0bed0cdc7cb9bfacb3ed6bcabbee5
Jethro Kuan <jethrokuan95@gmail.com>
2018-11-06 23:59:50 +0800

    fix lda export

--- traditional_models/lda.py
+++ traditional_models/lda.py
@@ -22,11 +22,14 @@ from dataset_tools.utils import read_file
 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.externals import joblib
 
 parser = ArgumentParser()
 
 parser.add_argument(
     "--data_path", help="Path to newline-delimited text file", required=True)
+parser.add_argument(
+    "--save_path", help="Path to save lda model.", required=True)
 
 args, _ = parser.parse_known_args()
 
@@ -47,7 +50,7 @@ t0 = time()
 
 n_features = 1000
 n_components = 10
-n_top_words = 20
+n_top_words = 10
 
 
 count_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=n_features, stop_words='english')
@@ -58,12 +61,15 @@ logging.info("count vectorizer completed in {} seconds".format(time() - t0))
 logging.info("Fitting LDA model with tf features, n_features={}".format(n_features))
 t0 = time()
 
-lda = LatentDirichletAllocation(n_components=n_components, max_iter=50,
+lda = LatentDirichletAllocation(n_components=n_components,
+                                max_iter=100,
                                 learning_method='online',
                                 learning_offset=50.,
                                 random_state=0)
 lda.fit(tf)
 logging.info("done in %0.3fs." % (time() - t0))
+
+joblib.dump(lda, args.save_path)
 
 print("\nTopics in Topic Model:")
 tf_feature_names = count_vectorizer.get_feature_names()
